d6c7702c928efcec46053876569878c8bd32d823
Dinesh Dhayfule <109637254+dineshdhayfule@users.noreply.github.com>
2024-02-06 20:42:10 +0530

    Add files via upload

---
+++ Quiz Application/questions.js
@@ -0,0 +1,79 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "Hypertext Preprocessor",
+        options: [
+            "Hypertext Preprocessor",
+            "Hypertext Programming",
+            "Hypertext Preprogramming",
+            "Hometext Preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        options: [
+            "eXtensible Markup Language",
+            "eXecutable Multiple Language",
+            "eXTra Multi-Program Language",
+            "eXamine Multiple Language"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Which of the following is correct about features of JavaScript?",
+        answer: "Both of the above",
+        options: [
+            "Javascript is complementary to and integrated with HTML.",
+            "Javascript is open and cross platform.",
+            "Both of the above",
+            "All of the above"
+        ]
+    },
+    //   {
+    //   numb: 7,
+    //   question: "Your Question is Here",
+    //   answer: "Correct answer of the question is here",
+    //   options: [
+    //     "Option 1",
+    //     "option 2",
+    //     "option 3",
+    //     "option 4"
+    //   ]
+    // },
+];
